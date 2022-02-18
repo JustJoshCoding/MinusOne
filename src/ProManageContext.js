a757fc76a2562b4c7ba6c70de61baac510f78c99
Joshua Obrien <joshua.obrien@my.uwi.edu>
2022-02-18 18:31:18 -0400

    Updating UI with firebase functionality

---
+++ src/ProManageContext.js
@@ -0,0 +1,40 @@
+import { createContext, useContext, useEffect, useState } from "react";
+import { onAuthStateChanged } from "firebase/auth";
+import { auth } from "./firebase";
+
+const ProManage = createContext();
+
+const ProManageContext = ({ children }) => {
+
+  const [alert, setAlert] = useState({
+    open: false,
+    message: "",
+    type: "success",
+  });
+  const [user, setUser] = useState(null);
+
+  useEffect(() => {
+    onAuthStateChanged(auth, (user) => {
+      if (user) setUser(user);
+      else setUser(null);
+    });
+  }, []);
+
+  return (
+    <ProManage.Provider
+      value={{
+        alert,
+        setAlert,
+        user,
+      }}
+    >
+      {children}
+    </ProManage.Provider>
+  );
+};
+
+export default ProManageContext;
+
+export const ProManageState = () => {
+  return useContext(ProManage);
+};
