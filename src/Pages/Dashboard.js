c04a505fd84e57cc8152c6e617dd2d1254d5233d
Joshua Obrien <joshua.obrien@my.uwi.edu>
2022-04-22 10:04:08 -0400

    final changes additional

--- src/Pages/Dashboard.js
+++ src/Pages/Dashboard.js
@@ -12,7 +12,10 @@ import LocalizationProvider from '@mui/lab/LocalizationProvider';
 import EventTracker from '../components/Dashboard/ShowEventTracker/EventTracker';
 import Timeline from '../components/Dashboard/ShowEventTracker/Timeline/Timeline'
 import { Container } from '@material-ui/core';
-
+import { ProManageState } from '../ProManageContext';
+import DashboardIcon from '@mui/icons-material/Dashboard';
+import Toolbar from '@mui/material/Toolbar';
+import IconButton from '@mui/material/IconButton';
 
 const Item = styled(Paper)(({ theme }) => ({
   backgroundColor: theme.palette.mode === 'dark' ? '#1A2027' : '#fff',
@@ -24,6 +27,7 @@ const Item = styled(Paper)(({ theme }) => ({
 
 export default function Dashboard() {
     const [value, setValue] = React.useState([null, null]);
+    const { user } = ProManageState();
 
     var showUpcommingEvents = (
         <React.Fragment>
@@ -66,19 +70,45 @@ export default function Dashboard() {
 
     return (
         <Container style={{ textAlign: "center" }}>
-            <Grid container spacing={{ xs: 5, md: 3 }} columns={{ xs: 8, sm: 8, md: 8 }} >
-                <Grid item xs={8} >
-                    <Item>{showUpcommingEvents}</Item>
-                </Grid>
+            <Box sx={{ flexGrow: 1}}>
+                <Toolbar>
+                <IconButton
+                    size="large"
+                    edge="start"
+                    color="inherit"
+                    aria-label="open drawer"
+                    sx={{ mr: 2 }}
+                >
+                    <DashboardIcon />
+                </IconButton>
+                <Typography
+                    variant="h6"
+                    noWrap
+                    component="div"            
+                    color= "gold"
+                    fontFamily= "Montserrat"
+                    fontWeight= "bold"
+                    sx={{ display: { xs: 'none', sm: 'block'} }}
+                >
+                    Dashboard
+                </Typography>
+                <Box sx={{ flexGrow: 1 }} />
+                </Toolbar>
+            
+                <Grid container spacing={{ xs: 5, md: 3 }} columns={{ xs: 8, sm: 8, md: 8 }} >
+                    <Grid item xs={8} >
+                        <Item>{showUpcommingEvents}</Item>
+                    </Grid>
 
-                <Grid item xs={8}  >
-                    <Item>{showCalendar}</Item>
-                </Grid>
-                <Grid item xs={8} >
+                    <Grid item xs={8}  >
+                        <Item>{showCalendar}</Item>
+                    </Grid>
+                    <Grid item xs={8} >
 
-                    <Item>{showTimeline}</Item>
+                        {user && <Item>{showTimeline}</Item>}
+                    </Grid>
                 </Grid>
-            </Grid>
+            </Box>
         </Container>
     );
 }
